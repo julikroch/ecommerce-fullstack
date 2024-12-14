dca2abd5e828f9ef321af13829483f488f4a1e01
Julian Krochmal <juliankrochmal@gmail.com>
2024-12-14 20:04:21 -0300

    Update Prettier configuration and format code style in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import "./globals.css";
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Next.js + TypeScript + Tailwind CSS",
-  description: "A starter template for Next.js with TypeScript and Tailwind CSS",
+  title: 'Next.js + TypeScript + Tailwind CSS',
+  description: 'A starter template for Next.js with TypeScript and Tailwind CSS',
 };
 
 export default function RootLayout({
